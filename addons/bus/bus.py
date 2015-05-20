eeac19a16eeae52cc29c1ee4296ca50e80af274f
igrandi <igrandi@ubiar.com>
2015-05-20 16:06:07 -0300

    New Relic [Bugs Fixed]

--- addons/bus/bus.py
+++ addons/bus/bus.py
@@ -189,7 +189,7 @@ class Controller(openerp.http.Controller):
     @openerp.http.route('/longpolling/poll', type="json", auth="public")
     def poll(self, channels, last, options=None):
         if config.get('newrelic_config_file', False):
-            newrelic.agent.ignore_transaction() 
+            newrelic_agent.ignore_transaction() 
         if options is None:
             options = {}
         if not dispatch:
