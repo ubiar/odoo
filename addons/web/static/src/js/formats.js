b5407091aa4e7154e5bfcac7167a0d889504da19
Tomás Del Bianco <tdelbianco@ubiar.com>
2016-06-03 17:50:34 -0300

    [FIX] web: search input of date
    https://github.com/odoo/odoo/commit/50b94ef079b2fd9f4e9201ea8f977fadab109aea

--- addons/web/static/src/js/formats.js
+++ addons/web/static/src/js/formats.js
@@ -252,17 +252,17 @@ instance.web.parse_value = function (value, descriptor, value_if_empty) {
         case 'progressbar':
             return instance.web.parse_value(value, {type: "float"});
         case 'datetime':
-            var datetime = moment(value, [date_pattern + ' ' + time_pattern, date_pattern_wo_zero + ' ' + time_pattern_wo_zero], true)
+            var datetime = moment(value, [date_pattern + ' ' + time_pattern, date_pattern_wo_zero + ' ' + time_pattern_wo_zero, moment.ISO_8601], true);
             if (datetime.isValid())
                 return instance.web.datetime_to_str(datetime.toDate());
             throw new Error(_.str.sprintf(_t("'%s' is not a correct datetime"), value));
         case 'date':
-            var date = moment(value, [date_pattern, date_pattern_wo_zero], true)
+            var date = moment(value, [date_pattern, date_pattern_wo_zero, moment.ISO_8601], true);
             if (date.isValid())
                 return instance.web.date_to_str(date.toDate());
             throw new Error(_.str.sprintf(_t("'%s' is not a correct date"), value));
         case 'time':
-            var time = moment(value, [time_pattern, time_pattern_wo_zero], true);
+            var time = moment(value, [time_pattern, time_pattern_wo_zero, moment.ISO_8601], true);
             if (time.isValid())
                 return instance.web.time_to_str(time.toDate());
             throw new Error(_.str.sprintf(_t("'%s' is not a correct time"), value));
--- addons/web/static/src/js/search.js
+++ addons/web/static/src/js/search.js
@@ -1278,7 +1278,16 @@ instance.web.search.DateField = instance.web.search.Field.extend(/** @lends inst
         return instance.web.date_to_str(facetValue.get('value'));
     },
     complete: function (needle) {
-        var m = moment(needle);
+        // Make sure the needle has a correct format before the creation of the moment object. See
+        // issue https://github.com/moment/moment/issues/1407
+        var t, v;
+        try {
+            t = (this.attrs && this.attrs.type === 'datetime') ? 'datetime' : 'date';
+            v = instance.web.parse_value(needle, {'widget': t});
+        } catch (e) {
+            return $.when(null);
+        }
+        var m = moment(v, t === 'datetime' ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD');
         if (!m.isValid()) { return $.when(null); }
         var d = m.toDate();
         var date_string = instance.web.format_value(d, this.attrs);
