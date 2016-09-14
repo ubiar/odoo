a71e1b3047737a740beb95cc06f561d5f6e10322
tdelbianco <tdelbianco@ubiar.com>
2016-09-14 10:51:30 -0400

    [FIX] Bug with jsonp and POST

--- addons/utm/models/ir_http.py
+++ addons/utm/models/ir_http.py
@@ -12,6 +12,6 @@ class ir_http(orm.AbstractModel):
     def _dispatch(self):
         response = super(ir_http, self)._dispatch()
         for var, dummy, cook in self.pool['utm.mixin'].tracking_fields():
-            if var in request.params and request.httprequest.cookies.get(var) != request.params[var]:
+            if hasattr(request, 'params') and var in request.params and request.httprequest.cookies.get(var) != request.params[var]:
                 response.set_cookie(cook, request.params[var], domain=self.get_utm_domain_cookies())
         return response
