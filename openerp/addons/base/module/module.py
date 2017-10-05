ddb66a32aee50759ad9b7c1f843c48feac9d8fae
Ivan Grandi <igrandi@ubiar.com>
2017-10-05 15:51:04 +0000

    [Fix] auto install

--- openerp/addons/base/module/module.py
+++ openerp/addons/base/module/module.py
@@ -658,12 +658,12 @@ class module(osv.osv):
                     # Desactivo todas las vistas de ese modulo para que no generen conflictos
                     self.pool.get('ir.ui.view').write(cr, uid, self.pool.get('ir.ui.view').search(cr, uid, [('model_data_id.module', '=', mod.name)]), {'active': False})
                     if terp.get('auto_uninstall_copy_to'):
-                        self.pool.get('ir.model.data').write(cr, uid, self.pool.get('ir.model.data').search((cr, uid, [('module', '=', mod.name)]), {'module': terp.get('auto_uninstall_copy_to')})
+                        self.pool.get('ir.model.data').write(cr, uid, self.pool.get('ir.model.data').search(cr, uid, [('module', '=', mod.name)]), {'module': terp.get('auto_uninstall_copy_to')})
                 if parse_version(terp.get('version', default_version)) > parse_version(mod.latest_version or default_version):
                     res[0] += 1
                 if updated_values:
                     self.write(cr, uid, mod.id, updated_values)
-            else:
+            else: 
                 mod_path = modules.get_module_path(mod_name)
                 if not mod_path:
                     continue
