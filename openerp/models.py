b035b4b3f9bd559034b4b8c75be10a4fd31ec58e
igrandi <igrandi@ubiar.com>
2018-11-15 15:05:40 +0000

    [Imp] m2o name_get origen

--- openerp/models.py
+++ openerp/models.py
@@ -3326,7 +3326,7 @@ class BaseModel(object):
             try:
                 values = {'id': record.id}
                 for name, field in name_fields:
-                    values[name] = field.convert_to_read(record[name], use_name_get, context={'special_origin': [self._name, record.id]})
+                    values[name] = field.convert_to_read(record[name], use_name_get, context={'special_origin': [self._name, record]})
                 result.append(values)
             except MissingError:
                 pass
