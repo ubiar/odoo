1c191b408d1f18670b0aaca2041b10d02d7cef6c
igrandi <igrandi@ubiar.com>
2019-02-07 14:30:32 +0000

    [Fix] Stock: Transfer Detail

--- addons/stock/wizard/stock_transfer_details.py
+++ addons/stock/wizard/stock_transfer_details.py
@@ -95,7 +95,7 @@ class stock_transfer_details(models.TransientModel):
                     processed_ids.append(prod.packop_id.id)
                 else:
                     pack_datas['picking_id'] = self.picking_id.id
-                    prod.packop_id = self.env['stock.pack.operation'].create(pack_datas)
+                    prod.packop_id = self.env['stock.pack.operation'].with_context(no_recompute=True).create(pack_datas)
                     processed_ids.append(prod.packop_id.id)
         # Delete the others
         packops = self.env['stock.pack.operation'].search(['&', ('picking_id', '=', self.picking_id.id), '!', ('id', 'in', processed_ids)])
