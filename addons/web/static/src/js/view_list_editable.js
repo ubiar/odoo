14fea76e113246151f8b0f6228f8d80f05b63b89
tdelbianco <tdelbianco@ubiar.com>
2020-08-27 00:49:21 +0000

    [FIX] web: don't change page before saving

--- addons/web/static/src/js/view_list_editable.js
+++ addons/web/static/src/js/view_list_editable.js
@@ -302,6 +302,17 @@
             return cells;
         },
         /**
+         * Prevent reloading content while an ongoing save
+         */
+        reload_content_when_ready: function() {
+            var self = this;
+            var self_super = this._super;
+            var original_arguments = arguments;
+            return this.saving_mutex.exec(function() {
+                return self_super.apply(self, original_arguments);
+            });
+        },
+        /**
          * If currently editing a row, resizes all registered form fields based
          * on the corresponding row cell
          */
