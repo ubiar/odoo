9e51025a7b4df6b405d418b53b3a308438930625
tdelbianco <tdelbianco@ubiar.com>
2023-05-11 18:59:33 +0000

    [IMP] web: onchanges -> no enviar html y binary

--- addons/web/static/src/js/view_form.js
+++ addons/web/static/src/js/view_form.js
@@ -485,6 +485,13 @@ instance.web.FormView = instance.web.View.extend(instance.web.form.FieldManagerM
                 field_values[parent_name] = parent_values;
             }
         }
+        // Evito que se envien los campos que pueden ser pesados ya que eso hace que funciona muy lento
+        // el sistema y son campos que no se suelen usar en un on_change
+        _.each(this.fields, f=>{
+            if (f.field !== undefined && ['html', 'binary'].includes(f.field.type)) {
+                field_values[f.name] = false;
+            }
+        });
         return field_values;
     },
 
